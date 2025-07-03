1107eeceb952da4f2f85bb484683adcdd9954bd0
vinithaAyyakkannu <142732733+vinithaAyyakkannu@users.noreply.github.com>
2025-07-03 10:24:11 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -45,25 +45,25 @@ function App() {
         <h2>Company Details Form</h2>
         <form>
           <label>Company Name</label>
-          <input name="company" value={formData.company} onChange={handleChange} placeholder="Company Name" />
+          <input name="company" value={formData.company} onChange={handleChange}  />
 
           <label>Name</label>
-          <input name="name" value={formData.name} onChange={handleChange} placeholder="Name" />
+          <input name="name" value={formData.name} onChange={handleChange}  />
 
           <label>Position</label>
-          <input name="position" value={formData.position} onChange={handleChange} placeholder="Position" />
+          <input name="position" value={formData.position} onChange={handleChange} />
 
           <label>Mobile</label>
-          <input name="phone" value={formData.phone} onChange={handleChange} placeholder="Mobile" />
+          <input name="phone" value={formData.phone} onChange={handleChange} />
 
           <label>Email</label>
-          <input name="email" value={formData.email} onChange={handleChange} placeholder="Email" />
+          <input name="email" value={formData.email} onChange={handleChange}  />
 
           <label>Website</label>
-          <input name="website" value={formData.website} onChange={handleChange} placeholder="Website" />
+          <input name="website" value={formData.website} onChange={handleChange}  />
 
           <label>Address</label>
-          <input name="address" value={formData.address} onChange={handleChange} placeholder="Address" />
+          <input name="address" value={formData.address} onChange={handleChange} />
 
           <label>Photo</label>
           <input type="file" name="photo" accept="image/*" onChange={handleChange} />
@@ -75,13 +75,13 @@ function App() {
       <div className="id-card-preview">
         <div className="id-card">
           <div className="card-header">
-            <h2 className="company-name">{formData.company || "Company Name"}</h2>
+            <h2 className="company-name">{formData.company }</h2>
           </div>
 
           <div className="card-content">
             {formData.photo && <img src={formData.photo} alt="avatar" className="photo" />}
-            <h3>{formData.name || "Your Name"}</h3>
-            <p>{formData.position || "Job Title"}</p>
+            <h3>{formData.name }</h3>
+            <p>{formData.position }</p>
 
             <div className="info">
               <p>📞 {formData.phone}</p>
